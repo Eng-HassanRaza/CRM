3d88d86b13bd2437757cd229442b846c85957992
hassan-raza <hassan.numl2013@gmail.com>
2021-12-02 15:53:42 +0500

    Email verification for signup

--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -6,12 +6,20 @@ Copyright (c) 2019 - present AppSeed.us
 # Create your views here.
 from django.contrib.auth.decorators import login_required
 from django.db import transaction
+from django.core.mail import EmailMessage
+from django.contrib.sites.shortcuts import get_current_site
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.contrib.auth.models import User
 from .forms import LoginForm, SignUpForm, UserForm, ProfileForm
 from django.contrib import messages
 from django.utils.translation import ugettext_lazy as _
 from .models import Profile
+from .tokens import account_activation_token
 
 
 def login_view(request):
@@ -43,15 +51,25 @@ def register_user(request):
     if request.method == "POST":
         form = SignUpForm(request.POST)
         if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get("username")
-            raw_password = form.cleaned_data.get("password1")
-            user = authenticate(username=username, password=raw_password)
-
-            msg = form.cleaned_data.get("email")
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            current_site = get_current_site(request)
+            mail_subject = 'Activate your CMS account.'
+            message = render_to_string('accounts/account_activation_email_template.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),
+            })
+            to_email = form.cleaned_data.get('email')
+            email = EmailMessage(
+                mail_subject, message, to=[to_email]
+            )
+            email.send()
+            # return HttpResponse('Please confirm your email address to complete the registration')
             success = True
-
-            return render(request, "accounts/verify_email.html", {"msg": msg, "success": success})
+            return render(request, "accounts/verify_email.html", {"msg": to_email, "success": success})
 
         else:
             msg = 'Form is not valid'
@@ -83,3 +101,21 @@ def update_profile(request):
         'user_form': user_form,
         'profile_form': profile_form
     })
+
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        # return redirect('home')
+        success = True
+        return render(request, "accounts/account_activated.html", {"success": success})
+    else:
+        success = False
+        return render(request, "accounts/invalid_email_verification_link.html", {"success": success})
--- core/settings.py
+++ core/settings.py
@@ -121,6 +121,10 @@ STATICFILES_DIRS = (
     os.path.join(CORE_DIR, 'apps/static'),
 )
 
-
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
 #############################################################
 #############################################################
